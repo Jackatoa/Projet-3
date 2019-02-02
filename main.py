4aec44ca6b1bb1a495face055951f927bc25fbc1
Jackatoa <xavier.endres@gmail.com>
2019-02-02 22:38:26 +0100

    rajout de fonctionnalités / pep 8

--- main.py
+++ main.py
@@ -3,41 +3,9 @@ from tile import Tile
 import niveaux
 import random
 
-"""
-AFFICHAGE
-    affichage de la map
-    affichage des objets
-    affichage du gardien
-    affichage de jack
-    affichage de l'inventaire'
-    affichage du score
-
-generation de la map
-    labyrinthe
-    position de base des personnages
-    position de base des objets
-
-
-
-Jeu
-    interaction avec les touches
-        checkmove
-
-    checkposition
-        position des objets
-        sortie
-        gardien
-        lave
-
-    refresh images
-        refresh de la position de mc gyver
-        refresh des objets
-        refresh du compteur
-        refresh du gardien
-        refresh des messages"""
-
 
 class Main:
+    """Contain all the main functions"""
     jack = Tile()
     jackstart = Tile()
     lava = Tile()
@@ -46,9 +14,13 @@ class Main:
     potion = Tile()
     puppet = Tile()
     exit = Tile()
+    invsyringe = Tile()
+    invpotion = Tile()
+    invpuppet = Tile()
     itemscollected = 0
 
     def map(self):
+        """Generate all the map tiles"""
         ordo = 0
         for x in niveaux.niveau2:
             absc = 0
@@ -69,6 +41,7 @@ class Main:
             ordo += 40
 
     def getposition(self, indice):
+        """Take the indice to return the coordinates """
         ordo = 0
         for x in niveaux.niveau2:
             absc = 0
@@ -79,10 +52,12 @@ class Main:
             ordo += 40
 
     def generationposition(self, indice):
+        """Set the coordinates of the specified tile"""
         x, y = m.getposition(indice)
         return x, y
 
     def generationobjets(self):
+        """Take 3 differents random coordinates for the items"""
         waylst = []
         ordo = 0
         for x in niveaux.niveau2:
@@ -98,25 +73,38 @@ class Main:
             for y in list(x):
                 itemslst2.append(y)
 
-        Main.syringe.x = itemslst2[0]
-        Main.syringe.y = itemslst2[1]
-        Main.potion.x = itemslst2[2]
-        Main.potion.y = itemslst2[3]
-        Main.puppet.x = itemslst2[4]
-        Main.puppet.y = itemslst2[5]
+        m.itemsposreset(itemslst2)
         m.displayitems()
 
+    def itemsposreset(self, list):
+        """Set the coordinates for the items"""
+        Main.syringe.x = list[0]
+        Main.syringe.y = list[1]
+        Main.potion.x = list[2]
+        Main.potion.y = list[3]
+        Main.puppet.x = list[4]
+        Main.puppet.y = list[5]
+        Main.invsyringe.x = -100
+        Main.invsyringe.y = -100
+        Main.invpotion.x = -100
+        Main.invpotion.y = -100
+        Main.invpuppet.x = -100
+        Main.invpuppet.y = -100
+
     def displayitems(self):
+        """Display items"""
         t.testdisplaytile("images/potion.png", 40, 40, Main.potion.x, Main.potion.y)
-        t.testdisplaytile("images/seringue.png", 40, 40, Main.puppet.x, Main.puppet.y)
-        t.testdisplaytile("images/poupee.png", 40, 40, Main.syringe.x, Main.syringe.y)
+        t.testdisplaytile("images/poupee.png", 40, 40, Main.puppet.x, Main.puppet.y)
+        t.testdisplaytile("images/seringue.png", 40, 40, Main.syringe.x, Main.syringe.y)
 
     def cleanitems(self):
+        """Delete the items tiles"""
         t.testdisplaytile("images/chemin1.png", 40, 40, Main.potion.x, Main.potion.y)
         t.testdisplaytile("images/chemin1.png", 40, 40, Main.puppet.x, Main.puppet.y)
         t.testdisplaytile("images/chemin1.png", 40, 40, Main.syringe.x, Main.syringe.y)
 
     def generationinit(self):
+        """Generate all the tiles needed to start the game"""
         m.map()
         m.generationobjets()
         Main.jackstart.x, Main.jackstart.y = m.generationposition("M")
@@ -125,12 +113,15 @@ class Main:
         Main.lava.x, Main.lava.y = m.generationposition("D")
         t.testdisplaytile("images/Gardien.png", 40, 40, Main.gardien.x, Main.gardien.y)
 
-
-
-    def game(self):
-        gamecontinue = True
+    def jackposreset(self):
+        """Reset the main character position to the start point"""
         Main.jack.x = Main.jackstart.x
         Main.jack.y = Main.jackstart.y
+
+    def game(self):
+        """Main function, make the game run"""
+        gamecontinue = True
+        m.jackposreset()
         x_change = 0
         y_change = 0
         while gamecontinue:
@@ -140,82 +131,127 @@ class Main:
                 if event.type == pygame.KEYUP:
                     x_change, y_change = m.keyinput(event, Main.jack.x, Main.jack.y)
             t.cleantile(Main.jack.x, Main.jack.y)
-            Main.jack.x += x_change
-            x_change = 0
-            Main.jack.y += y_change
-            y_change = 0
-            m.checkposition()
-            if m.checkposition():
+            x_change, y_change = m.updatejackpos(x_change, y_change)
+            m.refresh()
+            if m.checkexit():
                 gamecontinue = False
-            t.testdisplaytile("images/MacGyver.png", 40, 40, Main.jack.x, Main.jack.y)
             pygame.display.update()
 
+    def updatejackpos(self, x, y):
+        """Update jack coordinates after move"""
+        Main.jack.x += x
+        Main.jack.y += y
+        return 0, 0
+
+    def refresh(self):
+        """Get positions and refresh tiles and score"""
+
+        m.checkposition()
+        t.cleanscore()
+        m.score()
+        m.displayinventory()
+        t.testdisplaytile("images/MacGyver.png", 40, 40, Main.jack.x, Main.jack.y)
+
+    def score(self):
+        """Display the score"""
+        color = (255, 255, 255)
+        t.testgameDisplay.blit(
+            pygame.font.SysFont(None, 25).render("Objets ramassés: " + str(Main.itemscollected) +
+                                                 "/3",
+                                                 True,
+                                                 color),
+            (420, 570))
+
+    def keyinputcheck(self, x, y, addx, addy):
+        """Return modified coordonates if move is possible"""
+        if m.checkmove(x + addx, y + addy):
+            return addx, addy
+        else:
+            return 0, 0
+
     def keyinput(self, event, x, y):
+        """Return coordinates after move tentative"""
         if event.key == pygame.K_LEFT:
-            if m.checkmove(x - 40, y, ):
-                return -40, 0
-            else:
-                return 0, 0
+            return m.keyinputcheck(x, y, -40, 0)
         if event.key == pygame.K_RIGHT:
-            if m.checkmove(x + 40, y):
-                return 40, 0
-            else:
-                return 0, 0
+            return m.keyinputcheck(x, y, 40, 0)
         if event.key == pygame.K_UP:
-            if m.checkmove(x, y - 40):
-                return 0, -40
-            else:
-                return 0, 0
+            return m.keyinputcheck(x, y, 0, -40)
         if event.key == pygame.K_DOWN:
-            if m.checkmove(x, y + 40):
-                return 0, 40
-            else:
-                return 0, 0
+            return m.keyinputcheck(x, y, 0, 40)
 
     def itemiscollected(self, item):
-        # collecte les objets
+        """Store the status of items"""
         item.obtenu = True
         item.x = -100
         item.y = -100
         Main.itemscollected += 1
+        m.inventory(item)
 
-    def whereisjack(self, text, item, time):
-        # compare la position de Jack aux objets, affichant un message et les ramassants
+    def whereisjack(self, text, item, invitem, time):
+        """Check if Jack have picked an item"""
         if (Main.jack.x, Main.jack.y) == (item.x, item.y):
             t.message_display(text, time, 300, 40)
             m.itemiscollected(item)
+            m.inventory(invitem)
 
     def checkbattle(self):
-        # vérifie que les conditions requises sont remplies pour endormir le gardien
+        """Check if Jack have picked all the items to beat the guardian"""
         if Main.puppet.obtenu != True or Main.potion.obtenu != True or Main.syringe.obtenu != True:
             return True
 
-    def checkposition(self):
-        # compare la position de McGyver aux objets / personnages
-        m.whereisjack("Une poupée vaudou vampire ? Intéressant...", Main.puppet, 1)
-        m.whereisjack("Une bouteille rempli de sang... Mais du sang de quoi ?", Main.potion, 1)
-        m.whereisjack("Une seringue vide...", Main.syringe, 1)
-
+    def checkexit(self):
+        """Check if Jack have reach the exit"""
         if (Main.jack.x, Main.jack.y) == (Main.exit.x, Main.exit.y):
             t.message_display("Bravo Richard !", 2, 300, 40)
             return True
+
+    def battleloserestart(self):
+        """Reset items and jack"""
+        t.message_display("Mal préparé le gardien tire sur richard", 2, 300, 40)
+        m.cleanitems()
+        m.generationobjets()
+        m.jackposreset()
+        Main.itemscollected = 0
+        Main.syringe.obtenu = False
+        Main.potion.obtenu = False
+        Main.puppet.obtenu = False
+
+    def displayinventory(self):
+        """Display the inventory"""
+        color = (255, 255, 255)
+        t.testgameDisplay.blit(
+            pygame.font.SysFont(None, 25).render("Inventaire : ", True, color), (20, 570))
+        t.testdisplaytile("images/poupee.png", 30, 30, Main.invpuppet.x, Main.invpuppet.y)
+        t.testdisplaytile("images/seringue.png", 30, 30, Main.invsyringe.x, Main.invsyringe.y)
+        t.testdisplaytile("images/potion.png", 30, 30, Main.invpotion.x, Main.invpotion.y)
+
+    def getinventorypos(self):
+        """Return coordinates for inventory items"""
+        return (85 + 40 * Main.itemscollected), 565
+
+    def inventory(self, item):
+        """Set the coordinates for inventory items"""
+        item.x, item.y = m.getinventorypos()
+
+    def checkposition(self):
+        """Main function to check if Jack is on a same position as an event"""
+        m.whereisjack("Une poupée vaudou vampire ? Intéressant...", Main.puppet, Main.invpuppet, 1)
+        m.whereisjack("Une bouteille rempli de sang... ", Main.potion, Main.invpotion, 1)
+        m.whereisjack("Une seringue vide...", Main.syringe, Main.invsyringe, 1)
         if (Main.jack.x, Main.jack.y) == (Main.lava.x, Main.lava.y):
             t.message_display("Tu aurais du apporter des saucisses", 1, 300, 40)
-            Main.jack.x = Main.jackstart.x
-            Main.jack.y = Main.jackstart.y
+            m.jackposreset()
         if (Main.jack.x, Main.jack.y) == (Main.gardien.x, Main.gardien.y):
             if m.checkbattle():
-                t.message_display("Mal préparé le gardien tire sur richard", 2, 300, 40)
-                m.cleanitems()
-                m.generationobjets()
-                Main.jack.x = Main.jackstart.x
-                Main.jack.y = Main.jackstart.y
+                m.battleloserestart()
             else:
                 t.message_display("Le gardien prends sa pause café", 2, 300, 40)
                 Main.gardien.x = -100
                 Main.gardien.y = -100
 
     def checkmove(self, x, y):
+        """Check if the move is possible"""
         ordo = 0
         walllst = []
         for a in niveaux.niveau2:
--- tile.py
+++ tile.py
@@ -1,8 +1,12 @@
 import pygame
 import niveaux
 import time
+
+
 class Tile:
+    """Contain the display functions"""
     ID = 0
+
     def __init__(self):
         Tile.ID += 1
         self.ID = Tile.ID
@@ -11,23 +15,18 @@ class Tile:
         self.obtenu = False
         self.testgameDisplay = pygame.display.set_mode((600, 600))
 
-
-
-
     def gamedisplay(self, display_width, display_height):
+        """Display the main screen"""
         return pygame.display.set_mode((display_width, display_height))
 
     def text_objects(self, text, font):
-        white = (255,255,255)
+        """Generate the texts"""
+        white = (255, 255, 255)
         textsurface = font.render(text, True, white)
         return textsurface, textsurface.get_rect()
 
-    def messagebox(self):
-        # genere une boite pour afficher le message
-        t.displaytile("images/box.png", 600, 80, 0, 0)
-
     def message_display(self, text, temps, positionx, positiony):
-        # crée un message avec couleur, position et durée d'affichage
+        """Display the texts"""
         self.messagebox()
         largetext = pygame.font.Font('freesansbold.ttf', 20)
         textsurf, textrect = self.text_objects(text, largetext)
@@ -39,7 +38,7 @@ class Tile:
         self.cleanmessage()
 
     def cleanmessage(self):
-        # nettoie la boite message
+        """Clean the texts"""
         ordo = 0
         i = 0
         while i < 2:
@@ -62,41 +61,39 @@ class Tile:
             ordo += 40
             i += 1
 
-
-
     def messagebox(self):
-        # genere une boite pour afficher le message
+        """A box to display the texts"""
         self.testdisplaytile("images/box.png", 600, 80, 0, 0)
 
     def cleanscore(self):
-        # rafraichi les sprites dans la zone du score
+        """Clean the score"""
         ordo = 560
         lst = niveaux.niveau2[14]
         absc = 0
         for y in lst:
-            if y == "O":
+            if y == "#":
                 self.testdisplaytile("images/mur1.png", 40, 40, absc, ordo)
-            if y == "X":
+            if y == " ":
                 self.testdisplaytile("images/chemin1.png", 40, 40, absc, ordo)
             if y == "D":
                 self.testdisplaytile("images/lava.png", 40, 40, absc, ordo)
             if y == "A":
                 self.testdisplaytile("images/chemin1.png", 40, 40, absc, ordo)
             if y == "G":
-                self.testdisplaytile( "images/chemin1.png", 40, 40, absc, ordo)
+                self.testdisplaytile("images/chemin1.png", 40, 40, absc, ordo)
             if y == "M":
-                self.testdisplaytile( "images/chemin1.png", 40, 40, absc, ordo)
+                self.testdisplaytile("images/chemin1.png", 40, 40, absc, ordo)
             absc += 40
 
-
-
-
     def testdisplay(self, img, x, y):
+        """Display a tile"""
         return self.testgameDisplay.blit(img, (x, y))
 
     def testdisplaytile(self, img, a, b, x, y):
+        """Transform an img size to be displayed"""
         sizedImg = pygame.transform.scale(pygame.image.load(img), (a, b))
         return self.testdisplay(sizedImg, x, y)
 
     def cleantile(self, x, y):
+        """Clean the previous walked on tile"""
         self.testdisplaytile("images/chemin1.png", 40, 40, x, y)
